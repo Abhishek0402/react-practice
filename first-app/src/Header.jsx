3450048cc39bf9e536f7d23d25b0d127eba45714
AbhishekGupta0402 <abhishekedu4979@gmail.com>
2024-05-23 22:26:28 +0530

    added styling for header

--- first-app/src/Header.jsx
+++ first-app/src/Header.jsx
@@ -1,8 +1,10 @@
+import './Header.css';
+
 /* eslint-disable react/prop-types */
 export function HeaderComponent(props){
     return (
     <>
-      <h1>
+      <h1 className="header">
        {props.title} this is {props.children} server
       </h1>
     </>)
