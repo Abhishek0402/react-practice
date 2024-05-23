e89c43667ff6297a3bceed016611f14505f5b2b5
AbhishekGupta0402 <abhishekedu4979@gmail.com>
2024-05-23 21:42:02 +0530

    event handler

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -7,6 +7,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function handleClick(){
+    console.log('test event triggered');
+  }
+
   return (
     <>
       <div>
@@ -24,9 +28,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={handleClick}>Test event</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
