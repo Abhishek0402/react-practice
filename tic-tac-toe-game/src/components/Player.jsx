024c8eb85b11a0a5c632a3b8e06f6df7414e2a32
AbhishekGupta0402 <abhishekedu4979@gmail.com>
2024-05-26 00:10:17 +0530

    implemented player name edit functionallity

--- tic-tac-toe-game/src/components/Player.jsx
+++ tic-tac-toe-game/src/components/Player.jsx
@@ -3,25 +3,31 @@ import {useState} from 'react';
 /* eslint-disable react/prop-types */
 export default function Player({name,symbol}){
 const [isEditing,setIsEditing] = useState(false);
+const [playerName,setName] = useState(name);
 
     function updateIsEditing(){
-        setIsEditing(true);
+        setIsEditing(isEditing => !isEditing);
     }
 
+    function updateName(event){
+        setName(event.target.value);
+    }
     return (
     <li>
         <span className="player">
         {
         !isEditing ?
             <span className="player-name">
-                {name}
+                {playerName}
             </span> : 
-            <input type="text" />
+            <input type="text" value={playerName} onChange={updateName}/>
         }
         <span className="player-symbol">
             {symbol}
         </span>
-        <button onClick={updateIsEditing}>Edit</button>
+        <button onClick={updateIsEditing}>
+            {isEditing ? 'Save' : 'Edit'}
+        </button>
         </span>
     </li>
   );
